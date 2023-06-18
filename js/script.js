413e677f2d1cae8bd3a5514219d563a27c96e122
Francis Reyes <reyesfrancisp@gmail.com>
2023-06-18 00:07:43 -0400

    added some starter code

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+
+var baseURL = "https://api.openweathermap.org/data/2.5/";
+var specialURL = `weather?appid=${YOUR_API_KEY}&units=imperial&q=${CITY_NAME_FROM_INPUT},US`;
+//get from user input to search box
+
