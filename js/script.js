60614aef06319b79db977aa91af322ccf2d821f9
Francis Reyes <reyesfrancisp@gmail.com>
2023-06-18 15:44:01 -0400

    added weather URL

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 
 
 var baseURL = "https://api.openweathermap.org/data/2.5/";
-var specialURL = `weather?appid=${YOUR_API_KEY}&units=imperial&q=${CITY_NAME_FROM_INPUT},US`;
+var apiKey = "39d4c29ad2d7cc69817d5de93962e041";
+var cityName = "New York";
+var specialURL = `weather?appid=${apiKey}&units=imperial&q=${cityName},US`;
 //get from user input to search box
 
+var weatherURL = baseURL + specialURL;
+
+console.log(weatherURL);
